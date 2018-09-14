e7b6fe7a45039ce217fa46f8c6d8d475c8c334ea
AlbericoD <a.diasbarreto@icloud.com>
2018-09-14 14:54:48 -0300

    criado script main start na raiz do app, configurado o build do frontend para ser servidor com express

--- api/src/app.js
+++ api/src/app.js
@@ -7,12 +7,21 @@ const app = express();
 const { catchAll, notFound } = require('./error');
 const companyRouter = require('./company/company.router');
 const customerRouter = require('./customer/customer.router');
+const path = require('path');
 
 app.use(helmet());
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(
+  '/public',
+  express.static(path.join(__dirname, '../../frontend/build/'))
+);
+app.use(
+  '/static',
+  express.static(path.join(__dirname, '../../frontend/build/static/'))
+);
 app.use('/api/company', companyRouter);
 app.use('/api/customer', customerRouter);
 
